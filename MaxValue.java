67a51be0a0f11042b48d54186174f1235fe45bb1
Roshan4284 <roshanjha4284@gmail.com>
2025-04-22 18:31:12 +0530

    Add files via upload

---
+++ MaxValue.java
@@ -0,0 +1,15 @@
+public class MaxValue {
+    public static void main(String[] args) {
+        int a=12;
+        int b=14;
+        int max=0;
+        if(a>b){
+            max=a;
+        }
+        else{
+            max=b;
+        }
+        System.out.println("the value of max"+max);
+    }
+    
+}
